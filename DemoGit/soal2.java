faed6159be03a1ed37640626d4107cc513b96640
fathan1208 <fathannur595@gmail.com>
2021-10-17 20:29:22 +0700

    soal2.java

---
+++ DemoGit/soal2.java
@@ -0,0 +1,12 @@
+
+public class soal2{
+	public static void main (String[] args){
+		String nama;
+		int umur;
+		nama= "Indra Azimi.";
+		umur=20;
+
+System.out.println("Halo, " + nama );
+System.out.println("ternnyata usiamu baru" +" "+umur+" "+ "tahun ya!");
+    }
+}
