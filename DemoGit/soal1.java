9186a3a5d1dc52246b2939c9f985d7f62831b540
fathan1208 <fathannur595@gmail.com>
2021-10-17 20:28:04 +0700

    soal1.java

---
+++ DemoGit/soal1.java
@@ -0,0 +1,12 @@
+
+public class soal1{
+	public static void main (String[] args){
+
+System.out.println("Dira menabung : 10000" );
+int menabung = 10000;
+System.out.println("Selama 30 hari" );
+int selama = 30;
+
+System.out.println("Total :" +menabung*selama);
+	}
+}
